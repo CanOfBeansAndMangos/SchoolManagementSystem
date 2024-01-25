d375dd0c2641eb0528c2612390617ceaf29e4cd2
LordMangoz <umangabajgai@gmail.com>
2024-01-25 11:06:02 -0800

    I dont know

--- src/Teacher.java
+++ src/Teacher.java
@@ -1,7 +1,22 @@
+import javax.security.auth.Subject;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 public class Teacher extends Person
 {
-    public Teacher(String name, int age, String address)
+    ArrayList<String> subjects = new ArrayList<>();
+    public Teacher(String name, int age, String address, String subject)
     {
         super(name, age, address);
+        subjects.add(subject);
     }
+    public void addSubject(String subjectToAdd)
+    {
+        subjects.add(subjectToAdd);
+    }
+    public void removeSubject(String subjectToRemove)
+    {
+        subjects.remove(subjectToRemove);
+    }
+
 }
